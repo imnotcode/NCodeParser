9361e5938726635a7b08ff30cd7fa51df16cdf8d
Coder <ansh7938@gmail.com>
2019-12-04 09:14:38 +0900

    Add NovelExplorer Class (#18)

---
+++ NCodeParser/IO/NovelExplorer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCodeParser.IO
+{
+	public class NovelExplorer
+	{
+		public async void asdf()
+		{
+			using (var client = new CookieAwareWebClient())
+			{
+				client.Headers.Add("User-Agent: Other");
+				client.UseDefaultCredentials = true;
+
+				string URL = "";
+
+				var bytes = client.DownloadData(URL);
+				var downloadedString = Encoding.UTF8.GetString(bytes);
+			}
+		}
+	}
+}
